cbe75548a333e26240a6c89a75d67a839fd8b7f8
allytang <allytang2015@gmail.com>
2019-09-06 11:51:28 +1000

    Updates

---
+++ precache-manifest.15e410b54605e0f17b96ce07055940a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e249d2fe7602dc1d7a5bf00359a744ea",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "bb79e294cdfe89ef7333",
+    "url": "/react-recipe-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bb79e294cdfe89ef7333",
+    "url": "/react-recipe-app/static/js/2.67a5efff.chunk.js"
+  },
+  {
+    "revision": "8d8ed77976576db9c3e0",
+    "url": "/react-recipe-app/static/js/main.fdca10a5.chunk.js"
+  },
+  {
+    "revision": "0eb24de18e6ad11a0f02",
+    "url": "/react-recipe-app/static/js/runtime~main.ccc918fb.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-recipe-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "305eba92086731db4fa483eac13e5dc1",
+    "url": "/react-recipe-app/static/media/headerPig.305eba92.jpg"
+  }
+]);
